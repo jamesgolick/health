fa869ae42b02fcde3eeba0c3d2dd85e4dffa258c
James Golick <jamesgolick@gmail.com>
2012-11-27 11:31:26 -0800

    oops - s/Healthy/Health/

--- lib/health/endpoint.rb
+++ lib/health/endpoint.rb
@@ -5,19 +5,19 @@ module Health
     before do
       content_type "application/json"
 
-      if !instance_eval(&Healthy.endpoint_access_policy)
+      if !instance_eval(&Health.endpoint_access_policy)
         halt "Access denied."
       end
     end
 
     get "/" do
-      Healthy.names.to_json
+      Health.names.to_json
     end
 
     get "/:name" do
-      pass if !Healthy.names.include?(params[:name].to_sym)
+      pass if !Health.names.include?(params[:name].to_sym)
 
-      value = Healthy.perform(params[:name])
+      value = Health.perform(params[:name])
       status = value[:ok] ? 200 : 500
 
       [status, value.to_json]
